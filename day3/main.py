de9145c5be9c661d2037422f3691ffd73d4737a0
Riccardo Cherchi <richicherchi@gmail.com>
2021-12-06 01:55:13 +0100

    day 3 part 2

--- day3/main.py
+++ day3/main.py
@@ -1,38 +1,45 @@
 with open("data.txt") as f:
     lines = f.read().split("\n")
 
-    rates = []
-    ellips = []
+    stage = 0
 
-    # for i in li(nes:
-    #     count_0 = 0
-    #     count_1 = 0
-    #     for j in i:
-    #         if int(j):
-    #             count_1+= 1
-    #         else:
-    #             count_0 += 1
-    #     if count_0 > count_1:
-    #         rates.append(0)
-    #     else:
-    #         rates.append(1))
-    for i in range(0, len(lines[0])):
-        count_0 = 0
-        count_1 = 0
-        for j in lines:
-            if int(j[i]):
-                count_1 += 1
+    def filter_oxygen(stage, oxygen_list):
+
+        count_0 = []
+        count_1 = []
+        for i in oxygen_list:
+            if int(i[stage]):
+                count_1.append(i)
             else:
-                count_0 += 1
-            print(count_0, count_1)
-        if count_0 > count_1:
-            rates.append("0")
-            ellips.append("1")
+                count_0.append(i)
+        new_list = count_0 if len(count_0) > len(count_1) else count_1
+        print(new_list)
+        if len(new_list) > 1:
+            return filter_oxygen(stage + 1, new_list)
         else:
-            rates.append("1")
-            ellips.append("0")
+            return int(new_list[0], 2)
 
     
-    print(int("".join(rates), 2) * int("".join(ellips), 2))
+    def filter_co2(stage, co2_list):
 
+        count_0 = []
+        count_1 = []
+        for i in co2_list:
+            if int(i[stage]):
+                count_1.append(i)
+            else:
+                count_0.append(i)
+        new_list = count_1 if len(count_1) < len(count_0) else count_0
+        print(new_list)
+        if len(new_list) > 1:
+            return filter_co2(stage + 1, new_list)
+        else:
+            return int(new_list[0], 2)
+
+            
+    oxygen = filter_oxygen(stage, lines)
+    stage = 0
+    co2 = filter_co2(stage, lines)
+
+    print(oxygen * co2)
             
